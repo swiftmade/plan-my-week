5c6354699ae2835a8ccf93e9bbee3fbdcfc22b62
Ahmet Özışık <ozisikahmet@gmail.com>
2020-04-20 17:57:53 +0300

    fix drag handler, and disable selection

--- src/components/DragHandler.vue
+++ src/components/DragHandler.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="bg-gray-300" style="height:2px;cursor:ns-resize;"
-      @mousedown="startDrag()"></div>
+      @mousedown="startDrag()">
+
+    </div>
 </template>
 <script>
 import Vue from 'vue'
@@ -22,19 +24,22 @@ export default {
       this.dragging = true
       window.addEventListener('mouseup', this.stopDrag)
       window.addEventListener('mousemove', this.handleMove)
+
+      document.getElementsByTagName('body')[0].style = 'user-select: none;'
     },
     stopDrag () {
       Vue.set(this, 'dragging', false)
       Vue.set(this, 'lastY', null)
       window.removeEventListener('mouseup', this.stopDrag)
       window.removeEventListener('mousemove', this.handleMove)
+      document.getElementsByTagName('body')[0].style = 'user-select: inherit;'
     },
     handleMove ($event) {
       if (!this.dragging) {
         return
       }
 
-      const {bottom} = this.target.getBoundingClientRect()
+      const {bottom} = this.$el.getBoundingClientRect()
 
       if (this.lastY === null) {
         this.lastY = $event.clientY
